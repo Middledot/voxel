6be7205955c2fc8148d7f6d553397f4260498b29
Middledot <middledot.productions@gmail.com>
2023-11-23 16:28:00 -0500

    feat(raknet/datatypes): add more functions + refactor

--- src/raknet/objects/datatypes.rs
+++ src/raknet/objects/datatypes.rs
@@ -9,6 +9,10 @@
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use super::MsgBuffer;
+
+
+// long (64)
 pub fn from_i64_be_bytes(bytes: [u8; 8]) -> i64 {
     i64::from_be_bytes(bytes)
 }
@@ -17,6 +21,16 @@ pub fn to_i64_be_bytes(value: &i64) -> [u8; 8] {
     value.to_be_bytes()
 }
 
+pub fn from_u64_be_bytes(bytes: [u8; 8]) -> u64 {
+    u64::from_be_bytes(bytes)
+}
+
+pub fn to_u64_be_bytes(value: &u64) -> [u8; 8] {
+    value.to_be_bytes()
+}
+
+
+// int (32)
 pub fn from_i32_be_bytes(bytes: [u8; 4]) -> i32 {
     i32::from_be_bytes(bytes)
 }
@@ -25,6 +39,47 @@ pub fn to_i32_be_bytes(value: &i32) -> [u8; 4] {
     value.to_be_bytes()
 }
 
+pub fn from_u32_be_bytes(bytes: [u8; 4]) -> u32 {
+    u32::from_be_bytes(bytes)
+}
+
+pub fn to_u32_be_bytes(value: &u32) -> [u8; 4] {
+    value.to_be_bytes()
+}
+
+pub fn from_i32_le_bytes(bytes: [u8; 4]) -> i32 {
+    i32::from_le_bytes(bytes)
+}
+
+pub fn to_i32_le_bytes(value: &i32) -> [u8; 4] {
+    value.to_le_bytes()
+}
+
+pub fn from_i32_varint_bytes(buf: &mut MsgBuffer) -> i32 {
+    // taken from JSPrismarine
+    // resource: https://protobuf.dev/programming-guides/encoding/
+
+    let mut value = 0;
+    let mut c = 0;
+    loop {
+        let b = buf.read_byte();
+        value |= (b & 0x7f) << c;
+
+        if (b & 0x80) == 0 {
+            return value as i32;
+        }
+
+        c += 7;
+        if c <= 28 {
+            panic!("Varint length not within constraints")
+        }
+    }
+}
+
+
+
+
+// triad (24)
 pub fn from_u24_le_bytes_to_u32(bytes: [u8; 3]) -> u32 {
     let mut newarr = [0u8; 4];
     newarr[..3].copy_from_slice(&bytes[..3]);
@@ -40,6 +95,7 @@ pub fn to_u24_le_bytes(value: &u32) -> [u8; 3] {
     newarr
 }
 
+
 pub fn from_i16_be_bytes(bytes: [u8; 2]) -> i16 {
     i16::from_be_bytes(bytes)
 }
@@ -48,6 +104,7 @@ pub fn to_i16_be_bytes(value: &i16) -> [u8; 2] {
     value.to_be_bytes()
 }
 
+
 pub fn from_u16_be_bytes(bytes: [u8; 2]) -> u16 {
     u16::from_be_bytes(bytes)
 }
@@ -55,6 +112,12 @@ pub fn from_u16_be_bytes(bytes: [u8; 2]) -> u16 {
 pub fn to_u16_be_bytes(value: &u16) -> [u8; 2] {
     value.to_be_bytes()
 }
+
+
+pub fn to_u16_le_bytes(value: &u16) -> [u8; 2] {
+    value.to_le_bytes()
+}
+
 
 // TODO: test this (especially ipv6)
 #[allow(clippy::ptr_arg)]
@@ -66,6 +129,7 @@ pub fn from_address_bytes(version: u8, bytes: &Vec<u8>) -> SocketAddr {
         )
     } else if version == 0x06 {
         // new stuff taken from nukkit
+        // TODO: wiki.vg was also wrong about this
         // 0, 1 = family (0x17)
         // 2, 3 = port
         // 4, 5, 6, 7 = flow?
@@ -106,7 +170,7 @@ pub fn to_address_bytes(addr: &SocketAddr) -> Vec<u8> {
         address.extend_from_slice(&to_u16_be_bytes(&addr.port()));
     } else if let IpAddr::V6(ip) = addr.ip() {
         address.push(0x06);
-        address.extend_from_slice(&to_u16_be_bytes(&0x17));  // TODO: THIS IS LITTLE-ENDIAN
+        address.extend_from_slice(&to_u16_le_bytes(&0x17));
         address.extend_from_slice(&to_u16_be_bytes(&addr.port()));
 
         let octets = ip.octets();
@@ -118,6 +182,12 @@ pub fn to_address_bytes(addr: &SocketAddr) -> Vec<u8> {
 
     address
 }
+
+
+// double (float 64)
+// float (float 32)
+// TODO: implement these when you figure out if they're big endian or not
+
 
 pub fn get_unix_milis() -> u128 {
     let start = SystemTime::now();
